37d98d8d6592484d90197a3c1caedc868a7d2793
MartinGelevskiCodewell <martin.gelevski@codewell.mk>
2019-07-10 01:28:22 +0200

    Changes in city model IMPL to parse the city list and return a list of cities with Gson

--- app/src/main/java/com/martin/citysearch/main/CityModelImpl.java
+++ app/src/main/java/com/martin/citysearch/main/CityModelImpl.java
@@ -1,4 +1,88 @@
 package com.martin.citysearch.main;
 
-public class CityModelImpl {
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.martin.citysearch.map.OpenMap;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class CityModelImpl implements OpenMap.Model {
+
+    private static final String TAG = CityModelImpl.class.getSimpleName();
+    private final WeakReference<Context> context;
+    private final OpenMap.Presenter presenter;
+    private static final String FILE_NAME = "cities.json";
+    private List<City> cities;
+
+
+    public CityModelImpl(@NonNull OpenMap.Presenter presenter, @NonNull Context context) {
+        this.presenter = presenter;
+        this.context = new WeakReference<>(context);
+    }
+
+    @Override
+    public void getCityInfo() {
+        String aboutInfoJson = getCityInfoFromAssets();
+
+        if (aboutInfoJson != null && !aboutInfoJson.isEmpty()) {
+            List<City> cityList = parseCityInfo(aboutInfoJson);
+            if (cityList != null) {
+                presenter.onSuccess(cityList);
+                return;
+            }
+        }
+
+        presenter.onFail();
+
+    }
+
+    private List<City> parseCityInfo(String aboutInfoJson) {
+        Gson gson = new Gson();
+        /**
+         * with Gson I am taking the whole JSON file and  converting
+         * Java Objects into their JSON representation
+         */
+        Type collectionType = new TypeToken<List<City>>() {
+        }.getType();
+        cities = gson.fromJson(aboutInfoJson, collectionType);
+        Collections.sort(cities, new Comparator<City>() {
+            @Override
+            public int compare(City city1, City city2) {
+                return city1.getName().compareTo(city2.getName());
+            }
+        });
+        return cities;
+    }
+
+
+    private String getCityInfoFromAssets() {
+
+        if (context.get() != null) {
+            try {
+                AssetManager manager = context.get().getAssets();
+                InputStream file = manager.open(FILE_NAME);
+                byte[] formArray = new byte[file.available()];
+                file.read(formArray);
+                file.close();
+                return new String(formArray);
+            } catch (IOException ex) {
+                Log.e(TAG, ex.getLocalizedMessage(), ex);
+            }
+        }
+
+        return null;
+    }
+
 }
--- app/src/main/java/com/martin/citysearch/map/OpenMap.java
+++ app/src/main/java/com/martin/citysearch/map/OpenMap.java
@@ -1,4 +1,31 @@
 package com.martin.citysearch.map;
 
+import com.martin.citysearch.about.AboutInfo;
+import com.martin.citysearch.main.City;
+
+import java.util.List;
+
 public interface OpenMap {
+
+    interface Model {
+        void getCityInfo();
+    }
+
+
+    interface Presenter {
+        void getCityInfo();
+        void onSuccess(List<City> aboutInfo);
+        void onFail();
+    }
+
+
+    interface View {
+        void setCitName(String companyName);
+        void setCountryName(String companyAddress);
+        void setLongitude(String postalCode);
+        void setLatitude(String companyCity);
+        void showError();
+        void showProgress();
+        void hideProgress();
+    }
 }
